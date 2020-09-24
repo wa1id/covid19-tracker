c28e7deea35aa698a67cddf2a5b8762b2404c64b
walid <wyachou95@gmail.com>
2020-09-24 21:57:51 +0200

    Show value from API in Card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,7 +3,11 @@ import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
+
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -12,7 +16,7 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">{confirmed.value}</Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of active COVID-19 cases
