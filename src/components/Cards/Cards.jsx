4caff87c295312f7ef11fb9909323c19c5651711
walid <wyachou95@gmail.com>
2020-09-24 22:03:20 +0200

    use CountUp component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
 
 import styles from "./Cards.module.css";
 
@@ -16,7 +17,14 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">{confirmed.value}</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2}
+                separator="."
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
             <Typography variant="body2">
               Number of active COVID-19 cases
