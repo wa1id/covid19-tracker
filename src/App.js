b07d7be3ab7b93b6018c2d01e1a66314f88d1b23
walid <wyachou95@gmail.com>
2020-09-24 21:21:58 +0200

    Basic styling to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <Cards />
       <Chart />
       <CountryPicker />
